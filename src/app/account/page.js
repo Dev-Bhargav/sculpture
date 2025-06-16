478af0ddebb8c2cf9666f7797edfbe28735b9874
SPIDY <bhargavdinesh5406@gmail.com>
2025-06-16 16:07:38 +0530

    fix: account section syntax error

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -4,7 +4,6 @@ import { supabase } from "@/lib/supabaseClient";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { LogOut, User, User2 } from "lucide-react";
-import UserStatus from "../components/UserStatus";
 import Image from "next/image";
 
 export default function AccountPage() {
@@ -20,7 +19,6 @@ export default function AccountPage() {
       if (!user) {
         router.push("/login");
       } else {
-        const avatar_url = user.user_metadata?.avatar_url;
         setUser(user);
       }
     };
@@ -34,44 +32,46 @@ export default function AccountPage() {
   };
 
   if (!user) return <p className="p-6 text-center">Loading...</p>;
+  else {
+    avatar_url = user.user_metadata?.avatar_url;
+    return (
+      <div className="min-h-[80vh] flex flex-col items-center justify-center px-4 text-center">
+        <div className="bg-white rounded-lg p-8 w-full max-w-md">
+          <div className="flex flex-col items-center gap-2 mb-6">
+            {avatar_url ? (
+              <Image
+                width={100}
+                height={100}
+                src={avatar_url}
+                alt="User Avatar"
+                className="w-full h-full object-cover"
+                referrerPolicy="no-referrer"
+              />
+            ) : (
+              <User size={22} className="text-[var(--primary)] m-auto" />
+            )}
+            <h1 className="text-2xl font-bold">Welcome</h1>
+            <p className="text-gray-600">{user.email}</p>
+          </div>
 
-  return (
-    <div className="min-h-[80vh] flex flex-col items-center justify-center px-4 text-center">
-      <div className="bg-white rounded-lg p-8 w-full max-w-md">
-        <div className="flex flex-col items-center gap-2 mb-6">
-          {avatar_url ? (
-          <Image
-            width={100}
-            height={100}
-            src={avatar_url}
-            alt="User Avatar"
-            className="w-full h-full object-cover"
-            referrerPolicy="no-referrer"
-          />
-        ) : (
-          <User size={22} className="text-[var(--primary)] m-auto" />
-        )}
-          <h1 className="text-2xl font-bold">Welcome</h1>
-          <p className="text-gray-600">{user.email}</p>
-        </div>
+          <div className="flex flex-col gap-4">
+            <Link
+              href="/shop"
+              className="w-full py-2 rounded-md border border-[var(--primary)] text-[var(--primary)] font-medium hover:bg-[var(--primary)] hover:text-white transition-all text-center"
+            >
+              Shop Now
+            </Link>
 
-        <div className="flex flex-col gap-4">
-          <Link
-            href="/shop"
-            className="w-full py-2 rounded-md border border-[var(--primary)] text-[var(--primary)] font-medium hover:bg-[var(--primary)] hover:text-white transition-all text-center"
-          >
-            Shop Now
-          </Link>
-
-          <button
-            onClick={handleLogout}
-            className="w-full py-2 rounded-md border border-red-400 text-red-500 hover:bg-red-500 hover:text-white transition-all flex items-center justify-center gap-2 cursor-pointer"
-          >
-            <LogOut size={18} />
-            Log Out
-          </button>
+            <button
+              onClick={handleLogout}
+              className="w-full py-2 rounded-md border border-red-400 text-red-500 hover:bg-red-500 hover:text-white transition-all flex items-center justify-center gap-2 cursor-pointer"
+            >
+              <LogOut size={18} />
+              Log Out
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
